fbab7feffb466bc4a092395e605914c13bf35980
EduarChanaga <chanaga250@gmail.com>
2024-02-16 07:25:32 -0500

    modulos

---
+++ bonificaciones.py
@@ -0,0 +1,1 @@
+def bonificacion():
--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print()
+import usuario
+import servicios
+import reportes
+import bonificaciones
+print("Bienvenido")
+print("1. Usuario")
+print("2. Gestion de servicios")
+print("3. Reportes")
+print("4. Bonificacion clientes leales")
+decision=str("--> ")
+
+if decision=="1":
+    print(usuario.usuario)
+elif decision=="2":
+    print(servicios.servicios())
+elif decision=="3":
+    print(reportes.reportes()) 
+elif decision=="4":
+    print(bonificaciones.bonificacion())
