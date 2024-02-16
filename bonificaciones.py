8a5e66da9bfb99a03ad2a946d8c790c8515c4e06
EduarChanaga <chanaga250@gmail.com>
2024-02-16 09:57:42 -0500

    clientes fieles

--- bonificaciones.py
+++ bonificaciones.py
@@ -1,3 +1,4 @@
+import json
 with open("usuarios.json","r") as i:
     usuarios=json.load(i)
 def bonificacion():
@@ -7,5 +8,12 @@ def bonificacion():
     print("1. Comprobar clientes fieles")
     
     decision=str(input("--> "))
+    users=usuarios["movistar"]["usuarios"]
     if decision=="1":
+        año_actual=int(input("Ingrese año actual: "))
         print()
+        for i in users:
+            if i["Año"]<=año_actual:
+                i["Categoria"]="Cliente fiel"
+    with open("usuarios.json","w") as i:
+            json.dump(usuarios,i,indent=4)
